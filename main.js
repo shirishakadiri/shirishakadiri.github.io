da8b9a0e11933086a88e4bc4ade1ea707a138b44
shirishakadiri <shirishak04@gmail.com>
2020-05-02 22:49:28 -0400

    php send mail test

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 function projectDetails(name, details) {
-  document.getElementById("projectName").innerHTML = name;
-  document.getElementById("projectDetails").innerHTML = details;
+	document.getElementById("projectName").innerHTML = name;
+	document.getElementById("projectDetails").innerHTML = details;
 }
+
+function sendEmail() {
+Email.send({
+    Host : "smtp.mailtrap.io",
+    Username : "shirishak04@gmail.com",
+    Password : "france04",
+    To : 'shirishak04@gmail.com',
+    From : "shirishak04@gmail.com",
+    Subject : "Test email",
+    Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+}).then(
+  message => alert(message)
+);
+}
+
+//shirishak04@gmail.com
+//france04

ebe82d6393ca0b6a62f5478bfdc083bc92e303d1
shirishakadiri <shirishak04@gmail.com>
2020-05-03 23:20:53 -0400

    hover on projects

--- main.js
+++ main.js
@@ -3,19 +3,12 @@ function projectDetails(name, details) {
 	document.getElementById("projectDetails").innerHTML = details;
 }
 
-function sendEmail() {
-Email.send({
-    Host : "smtp.mailtrap.io",
-    Username : "shirishak04@gmail.com",
-    Password : "france04",
-    To : 'shirishak04@gmail.com',
-    From : "shirishak04@gmail.com",
-    Subject : "Test email",
-    Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-}).then(
-  message => alert(message)
-);
-}
-
-//shirishak04@gmail.com
-//france04
+$(document).ready(function(){
+  $(".card").hover(function(){
+    $(this).children(".project-img").css("opacity", "0.2");
+    $(this).children(".skills-div").css("display", "block");
+    }, function(){
+    $(this).children(".project-img").css("opacity", "1");
+    $(this).children(".skills-div").css("display", "none");
+  });
+});
